71f3e68b649b37e98daa6c89075b1c30d5b8fab6
Yasser <yasserfedsi34@gmail.com>
2025-02-26 13:32:30 +0100

    Refactor server.js to load environment variables directly and change default port to 5000

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const postgres = require("postgres");
-const env = require("dotenv").config();
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
@@ -253,7 +253,7 @@ app.delete("/api/sub-components/:uuid", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
